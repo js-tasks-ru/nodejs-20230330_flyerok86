233a269f9836a6aceabf2b71ee4f44ebf8a8cdb2
Pavel S <21025468+PASHANSX@users.noreply.github.com>
2023-04-18 20:05:48 +0300

    HW5 done

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,19 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    ctx.body = await new Promise(resolve=>subscribers.push(resolve));
 });
 
 router.post('/publish', async (ctx, next) => {
+    if(ctx.request.body.message) {
+        subscribers.forEach(resolve => {
+            resolve(ctx.request.body?.message)
+        });
+    }
+    ctx.status = 201;
 });
 
 app.use(router.routes());
