59ce8cad25847abc6d6cab9456ddcd6855441124
flyerok <21025468+PASHANSX@users.noreply.github.com@users.noreply.github.com>
2023-04-28 18:42:35 +0300

    HW6 done

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,12 @@
+const Category = require("../models/Category");
+const mapperCategory = require("../mappers/category");
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+
+  const normalize = categories.map((category) => {
+    return mapperCategory(category);
+  });
+
+  ctx.body = { categories: normalize };
 };
