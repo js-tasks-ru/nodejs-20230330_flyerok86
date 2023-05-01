18de6243435681df986f30713895d14614d4d40c
flyerok <21025468+PASHANSX@users.noreply.github.com@users.noreply.github.com>
2023-05-01 15:44:49 +0300

    fix

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    }
 });
 
 const categorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    subcategories:[subCategorySchema],
 });
 
 module.exports = connection.model('Category', categorySchema);
